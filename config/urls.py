09343ef496c2026abcc154207bafcb3080a52b71
saparbayev-azizbek-12 <saparbayev.azizbek12@gmail.com>
2026-03-14 10:51:26 +0500

    some changes

--- config/urls.py
+++ config/urls.py
@@ -8,11 +8,15 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('secure/admin/', admin.site.urls),
+    path('secure/part/', admin.site.urls),
     path('blog/', include('blog.urls', namespace='blog')),
     path('', include('core.urls', namespace='core')),
 ]
 
-# Serve media files in development
+# Custom 404 handler
+handler404 = 'django.views.defaults.page_not_found'
+
+# Serve media and static files in development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
